12e48c13dc87f309dd36342be7968ceffd5def7c
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-12-18 12:31:17 +0530

    init

---
+++ src/main/java/hibernate/training/Runner.java
@@ -0,0 +1,30 @@
+package hibernate.training;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import hibernate.training.entity.Employee;
+
+public class Runner {
+	public static void main(String[] args) {
+		// Create typesafe ServiceRegistry object
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		Employee e1 = new Employee();
+		e1.setId(101);
+		e1.setFirstName("Gaurav");
+		session.save(e1);
+		t.commit();
+		System.out.println("successfully saved");
+		factory.close();
+		session.close();
+	}
+}
--- src/main/java/hibernate/training/entity/Employee.java
+++ src/main/java/hibernate/training/entity/Employee.java
@@ -1,6 +1,30 @@
 package hibernate.training.entity;
 
 public class Employee {
+	private int id;  
+	private String firstName,lastName;
+	public Employee() {}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 
 } 
  
