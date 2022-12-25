d0fc9e9284270d4f71ee42d6e176c29309284a27
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-12-25 13:43:26 +0530

    HQL

--- src/main/java/hibernate/training/Runner.java
+++ src/main/java/hibernate/training/Runner.java
@@ -1,7 +1,10 @@
 package hibernate.training;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import hibernate.training.entity.Employee;
 
@@ -9,35 +12,54 @@ public class Runner {
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		
-		save(session);
-		fetch(session);
+			save(session);
+			fetch(session);
 		
 		session.close();
 	}
 
 	private static void fetch(Session session) {
-		Employee emp = session.get(Employee.class, 1);
-		System.out.println(emp);
-		session.close();
+		Query<Employee> query = session.createQuery("From Employee", Employee.class);
+		List<Employee> resultList = query.getResultList();
+		resultList.forEach(System.out::println);
 	}
 
 	private static void save(Session session) {
-		// State - transient
-		Employee e1 = new Employee();
-		//e1.setId(101);
-		e1.setFirstName("Gaurav");
-		e1.setFirstName("Gaurav1");
-		System.out.println("transient ....");
-		
 		// @formatter:off
 		Transaction tx = session.beginTransaction();
-			System.out.println("persistent ....");
-			//System.out.println(session.save(e1));
+			Employee e1 = new Employee();
+			e1.setFirstName("Guest_1");
+			e1.setLastName("DUCAT_1");
 			session.persist(e1);
-			// e1.setFirstName("Gaurav1"); //No operation perform as object is not dirty. 
-			e1.setFirstName("Gaurav2");
 			System.out.println("successfully saved");
 		tx.commit();
+		
+		tx = session.beginTransaction();
+			e1 = new Employee();
+			e1.setFirstName("Guest_2");
+			e1.setLastName("DUCAT_2");
+			session.persist(e1);
+			System.out.println("successfully saved");
+		tx.commit();
+		
+		
+		tx = session.beginTransaction();
+			e1 = new Employee();
+			e1.setFirstName("Guest_3");
+			e1.setLastName("DUCAT_3");
+			session.persist(e1);
+			System.out.println("successfully saved");
+		tx.commit();
+	
+		tx = session.beginTransaction();
+			e1 = new Employee();
+			e1.setFirstName("Guest_4");
+			e1.setLastName("DUCAT_4");
+			session.persist(e1);
+			System.out.println("successfully saved");
+		tx.commit();
+		
+		
 		// @formatter:on
 		
 		System.out.println("successfully operation completed");
