d4dd432d8dc5b13e704ad0a56cb31c717faefc6c
Saxena, Ashwani <ashwani_saxena@optum.com>
2022-12-26 20:41:57 +0530

    update

--- src/main/java/hibernate/training/Runner.java
+++ src/main/java/hibernate/training/Runner.java
@@ -8,7 +8,7 @@ import hibernate.training.entity.Employee;
 public class Runner {
 	public static void main(String[] args) {
 		// Create typesafe ServiceRegistry object
-		Session session = HibernateUtil.getSession();
+		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		// @formatter:off
 		Transaction tx = session.beginTransaction();
@@ -29,7 +29,6 @@ public class Runner {
 		
 		tx = session.beginTransaction();
 			e1 = new Employee();
-			e1 = new Employee();
 			e1.setId(103);
 			e1.setFirstName("Gaurav_3");
 			session.save(e1);
@@ -39,7 +38,7 @@ public class Runner {
 		tx = session.beginTransaction();
 			e1 = new Employee();
 			e1 = new Employee();
-			e1.setId(101);
+			e1.setId(104);
 			e1.setFirstName("Gaurav_4");
 			session.save(e1);
 			System.out.println("successfully saved");
