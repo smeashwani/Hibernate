e8c18ea862b2134ed7917f5bfdd98b22b596f92a
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-07-03 22:40:37 +0530

    update

--- src/main/java/hibernate/training/Runner.java
+++ src/main/java/hibernate/training/Runner.java
@@ -8,43 +8,44 @@ import hibernate.training.entity.Employee;
 public class Runner {
 	public static void main(String[] args) {
 		// Create typesafe ServiceRegistry object
-		Session session = HibernateUtil.getSessionFactory().openSession();
+		try (Session session = HibernateUtil.getSessionFactory().openSession()){
 
-		// @formatter:off
-		Transaction tx = session.beginTransaction();
-			Employee e1 = new Employee();
-			e1.setId(101);
-			e1.setFirstName("Gaurav");
-			session.save(e1);
-			System.out.println("successfully saved");
-		tx.commit();
-
-		tx = session.beginTransaction();
-			e1 = new Employee();
-			e1.setId(102);
-			e1.setFirstName("Gaurav_2");
-			session.save(e1);
-			System.out.println("successfully saved");
-		tx.commit();
-		
-		tx = session.beginTransaction();
-			e1 = new Employee();
-			e1.setId(103);
-			e1.setFirstName("Gaurav_3");
-			session.save(e1);
-			System.out.println("successfully saved");
-		tx.commit();
-		
-		tx = session.beginTransaction();
-			e1 = new Employee();
-			e1 = new Employee();
-			e1.setId(104);
-			e1.setFirstName("Gaurav_4");
-			session.save(e1);
-			System.out.println("successfully saved");
-		tx.commit();
-		// @formatter:on
-		session.close();
+			// @formatter:off
+			Transaction tx = session.beginTransaction();
+				Employee e1 = new Employee();
+				e1.setId(101);
+				e1.setFirstName("Gaurav");
+				session.save(e1);
+				System.out.println("successfully saved");
+			tx.commit();
+			
+	
+			tx = session.beginTransaction();
+				e1 = new Employee();
+				e1.setId(102);
+				e1.setFirstName("Gaurav_2");
+				session.save(e1);
+				System.out.println("successfully saved");
+			tx.commit();
+			
+			tx = session.beginTransaction();
+				e1 = new Employee();
+				e1.setId(103);
+				e1.setFirstName("Gaurav_3");
+				session.save(e1);
+				System.out.println("successfully saved");
+			tx.commit();
+			
+			tx = session.beginTransaction();
+				e1 = new Employee();
+				e1 = new Employee();
+				e1.setId(104);
+				e1.setFirstName("Gaurav_4");
+				session.save(e1);
+				System.out.println("successfully saved");
+			tx.commit();
+			// @formatter:on
+		}
 		System.out.println("successfully operation completed");
 	}
 }
