7034622410390b9898381d5cbc10cffcb023b7b5
smeashwani <104685977+smeashwani@users.noreply.github.com>
2023-07-08 10:31:05 +0530

    Update Runner.java

--- src/main/java/hibernate/training/Runner.java
+++ src/main/java/hibernate/training/Runner.java
@@ -7,39 +7,38 @@ import hibernate.training.entity.Employee;
 
 public class Runner {
 	public static void main(String[] args) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		
-		save(session);
-		fetch(session);
-		
-		session.close();
+		save();
+		fetch();
 	}
 
-	private static void fetch(Session session) {
-		Employee emp = session.get(Employee.class, 1);
-		System.out.println(emp);
-		session.close();
+	private static void fetch() {
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			System.out.println("Fetching");
+			Employee emp = session.load(Employee.class, 1);
+			System.out.println("Fetching");
+			System.out.println(emp);
+		}
 	}
 
-	private static void save(Session session) {
-		// State - transient
-		Employee e1 = new Employee();
-		//e1.setId(101);
-		e1.setFirstName("Gaurav");
-		e1.setFirstName("Gaurav1");
-		System.out.println("transient ....");
-		
-		// @formatter:off
-		Transaction tx = session.beginTransaction();
-			System.out.println("persistent ....");
-			//System.out.println(session.save(e1));
-			session.persist(e1);
-			// e1.setFirstName("Gaurav1"); //No operation perform as object is not dirty. 
-			e1.setFirstName("Gaurav2");
-			System.out.println("successfully saved");
-		tx.commit();
-		// @formatter:on
-		
+	private static void save() {
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			// State - transient
+			Employee e1 = new Employee();
+			e1.setFirstName("Gaurav");
+			e1.setFirstName("Gaurav1");
+			System.out.println("transient ....");
+			
+			// @formatter:off
+			Transaction tx = session.beginTransaction();
+				System.out.println("persistent ....");
+				//System.out.println(session.save(e1));
+				session.persist(e1);
+				//e1.setFirstName("Gaurav1"); //No operation perform as object is not dirty. 
+				e1.setFirstName("Gaurav2");
+				System.out.println("successfully saved");
+			tx.commit();
+			// @formatter:on
+		}
 		System.out.println("successfully operation completed");
 		
 	}
