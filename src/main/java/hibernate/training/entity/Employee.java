4c1a784784edc38b0afe47941003514321e6c7b5
smeashwani <104685977+smeashwani@users.noreply.github.com>
2023-10-29 11:55:40 +0530

    update

--- src/main/java/hibernate/training/entity/Employee.java
+++ src/main/java/hibernate/training/entity/Employee.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
+import lombok.Data;
 @NamedQueries(  
 	    {  
 	        @NamedQuery(  
@@ -18,44 +19,19 @@ import jakarta.persistence.Table;
 )
 @Entity
 @Table(name="emp")
+@Data
 public class Employee {
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.AUTO)
 	private int id;
-	
 	@Column(name="first_Name")
 	private String firstName;
-	
 	private String lastName;
-	
 	public Employee() {}
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
-	
-	
-
-} 
- 
+}
